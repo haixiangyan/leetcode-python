7f283d48f0ab744a82c9c8b6a1d2db0ac9476242
Haixiang6123 <haixiang6123@gmail.com>
2019-07-14 13:22:42 -0700

    Solve 38. Search a 2D Matrix II

---
+++ US Giants/Binary Search/38. Search a 2D Matrix II.py
@@ -0,0 +1,27 @@
+class Solution:
+    """
+    @param matrix: A list of lists of integers
+    @param target: An integer you want to search in matrix
+    @return: An integer indicate the total occurrence of target in the given matrix
+    """
+    def searchMatrix(self, matrix, target):
+        if matrix is None or len(matrix) == 0 or len(matrix[0]) == 0:
+            return 0
+
+        rows, cols = len(matrix), len(matrix[0])
+
+        x, y = rows - 1, 0
+        count = 0
+        while x >= 0 and y < cols:
+            if matrix[x][y] < target:
+                # Shift right
+                y += 1
+            elif matrix[x][y] > target:
+                # Shift top
+                x -= 1
+            else:
+                x -= 1
+                y += 1
+                count += 1
+
+        return count
