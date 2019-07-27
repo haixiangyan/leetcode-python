40192420065e1ac35ecd2b2dd9d231e5c9e0a8e6
Haixiang6123 <haixiang6123@gmail.com>
2019-07-26 21:35:06 -0700

    Solve 102. Linked List Cycle

--- US Giants/Linked List/102. Linked List Cycle.py
+++ US Giants/Linked List/102. Linked List Cycle.py
@@ -1,16 +1,15 @@
-"""
-Definition of ListNode
 class ListNode(object):
     def __init__(self, val, next=None):
         self.val = val
         self.next = next
-"""
+
 
 class Solution:
     """
     @param head: The first node of linked list.
     @return: True if it has a cycle, or false
     """
+
     def hasCycle(self, head):
         if head is None or head.next is None:
             return False
