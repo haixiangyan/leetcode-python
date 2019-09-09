53ae6db0372d6445feae1f7ba0c0567ee56b4244
Haixiang <haixiang6123@gmail.com>
2019-09-08 17:05:37 -0700

    Solve 109. Triangle

---
+++ US Giants/Dynamic Programming/109. Triangle.py
@@ -0,0 +1,19 @@
+class Solution:
+    """
+    @param triangle: a list of lists of integers
+    @return: An integer, minimum path sum
+    """
+    def minimumTotal(self, triangle):
+        n = len(triangle)
+        dp = [[0] * (i + 1) for i in range(n)]
+
+        dp[0][0] = triangle[0][0]
+        for i in range(1, n):
+            dp[i][0] = dp[i - 1][0] + triangle[i][0]
+            dp[i][i] = dp[i - 1][i - 1] + triangle[i][i]
+
+        for i in range(2, n):
+            for j in range(1, i):
+                dp[i][j] = min(dp[i - 1][j - 1], dp[i - 1][j]) + triangle[i][j]
+
+        return min(dp[n - 1])
