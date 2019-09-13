0e510cffda257ff6b04c105cc87abe2fd2d7b787
Haixiang6123 <haixiang6123@gmail.com>
2019-09-12 20:32:14 -0700

    Solve 4. Ugly Number II

---
+++ US Giants/Data Structure/4. Ugly Number II.py
@@ -0,0 +1,20 @@
+import heapq
+
+class Solution:
+    """
+    @param n: An integer
+    @return: return a  integer as description.
+    """
+    def nthUglyNumber(self, n):
+        visited = set([1])
+        heap = []
+
+        val = None
+        for i in range(n):
+            val = heapq.heappop(heap)
+            for factor in [2, 3, 5]:
+                if val * factor not in visited:
+                    visited.add(val * factor)
+                    heapq.heappush(heap, val * factor)
+
+        return val
