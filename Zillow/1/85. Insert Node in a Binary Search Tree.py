a9fba8123176568014904a5ffa003a32bc76e50d
Haixiang6123 <haixiang6123@gmail.com>
2019-09-29 22:43:48 -0700

    Solve 85. Insert Node in a Binary Search Tree

---
+++ Zillow/1/85. Insert Node in a Binary Search Tree.py
@@ -0,0 +1,22 @@
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+
+
+class Solution:
+    """
+    @param: root: The root of the binary search tree.
+    @param: node: insert this node into the binary search tree
+    @return: The root of the new binary search tree.
+    """
+    def insertNode(self, root, node):
+        if root is None:
+            return node
+
+        if root.val > node.val:
+            root.left = self.insertNode(root.left, node)
+        else:
+            root.right = self.insertNode(root.right, node)
+        
+        return root
