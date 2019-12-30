a1b267b8313eee44998771dc116a16b5ac735818
Haixiang6123 <haixiang6123@gmail.com>
2019-12-31 07:19:01 +0800

    Solved 115. Unique Paths II

---
+++ Amazon 2019秋招面试高频题/1 - Amazon 电面/115. Unique Paths II.py
@@ -0,0 +1,23 @@
+class Solution:
+    """
+    @param obstacleGrid: A list of lists of integers
+    @return: An integer
+    """
+    def uniquePathsWithObstacles(self, obstacleGrid):
+        dp = obstacleGrid
+
+        for i in range(len(dp)):
+            for j in range(len(dp[0])):
+                if i == 0 and j == 0:
+                    dp[i][j] = 1 - dp[i][j]
+                elif i == 0:
+                    dp[i][j] = 0 if dp[i][j] == 1 else dp[i][j - 1]
+                elif j == 0:
+                    dp[i][j] = 0 if dp[i][j] == 1 else dp[i - 1][j]
+                else:
+                    dp[i][j] = 0 if dp[i][j] == 1 else dp[i - 1][j] + dp[i][j - 1]
+
+        if dp[-1][-1] > 2147483647:
+            return -1
+
+        return dp[-1][-1]
