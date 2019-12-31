bdf00e92856840a412456ae2f1ca76f7dc9b71c5
Haixiang6123 <haixiang6123@gmail.com>
2019-12-31 14:59:08 +0800

    Solved 423. Valid Parentheses

---
+++ Amazon 2019秋招面试高频题/2 - Amazon Onsite/423. Valid Parentheses.py
@@ -0,0 +1,23 @@
+class Solution:
+    """
+    @param s: A string
+    @return: whether the string is a valid parentheses
+    """
+    def isValidParentheses(self, s):
+        if not s:
+            return True
+
+        stack = []
+
+        for char in s:
+            if char == '(' or char == '[' or char == '{':
+                stack.append(char)
+            else:
+                if not stack:
+                    return False
+                if char == ')' and stack[-1] != '(' or char == '}' and stack[-1] != '{' or char == ']' and stack[-1] != '[':
+                    return False
+
+                stack.pop()
+
+        return not stack
