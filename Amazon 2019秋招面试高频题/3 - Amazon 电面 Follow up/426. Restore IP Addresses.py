e0bafdc2a62471d5c0b8477c76510badf35a1c80
Haixiang6123 <haixiang6123@gmail.com>
2020-01-14 08:56:17 -0800

    Solved 426. Restore IP Addresses

---
+++ Amazon 2019秋招面试高频题/3 - Amazon 电面 Follow up/426. Restore IP Addresses.py
@@ -0,0 +1,24 @@
+class Solution:
+    """
+    @param s: the IP string
+    @return: All possible valid IP addresses
+    """
+    def restoreIpAddresses(self, s):
+        ips = []
+
+        self.dfs(s, 0, '', ips)
+
+        return ips
+
+    def dfs(self, s, part, ip, ips):
+        if part == 4:
+            if s == '':
+                ips.append(ip[1:])
+                return
+
+        for i in range(1, 4):
+            if i <= len(s):
+                if int(s[:i]) <= 255:
+                    self.dfs(s[i:], part + 1, ip + '.' + s[:i], ips)
+                if s[0] == '0':
+                    break
