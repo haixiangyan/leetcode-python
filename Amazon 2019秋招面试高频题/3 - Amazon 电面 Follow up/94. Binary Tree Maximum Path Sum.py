777d7b5b73575268d04c31860f2c049f235099ec
Haixiang6123 <haixiang6123@gmail.com>
2020-01-14 11:25:07 -0800

    Solved 94. Binary Tree Maximum Path Sum

---
+++ Amazon 2019秋招面试高频题/3 - Amazon 电面 Follow up/94. Binary Tree Maximum Path Sum.py
@@ -0,0 +1,25 @@
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+
+class Solution:
+    """
+    @param root: The root of binary tree.
+    @return: An integer
+    """
+    def maxPathSum(self, root):
+        max_sum, _ = self.divide_conquer(root)
+        return max_sum
+
+    def divide_conquer(self, root):
+        if root is None:
+            return float('-inf'), 0
+
+        left_max_sum, left_single = self.divide_conquer(root.left)
+        right_max_sum, right_single = self.divide_conquer(root.right)
+
+        max_sum = max(left_max_sum, right_max_sum, left_single + root.val + right_single)
+        single = max(left_single + root.val, right_single + root.val, 0)
+
+        return max_sum, single
