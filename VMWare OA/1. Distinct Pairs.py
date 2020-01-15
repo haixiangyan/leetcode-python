86b7d274779d9e419e023e1771ab7693ff8dafbe
Haixiang6123 <haixiang6123@gmail.com>
2020-01-14 16:55:01 -0800

    Update Distinct Pairs

--- VMWare OA/1. Distinct Pairs.py
+++ VMWare OA/1. Distinct Pairs.py
@@ -1,8 +1,8 @@
 class Solution:
     def distinct_pairs(self, nums, target):
-        pairs = []
+        counts = 0
         if not nums:
-            return pairs
+            return counts
 
         sorted_nums = sorted(nums)
         left, right = 0, len(sorted_nums) - 1
@@ -14,7 +14,7 @@ class Solution:
                 left += 1
             else:
                 # Add pair
-                pairs.append((sorted_nums[left], sorted_nums[right]))
+                counts += 1
                 # Move left and right to the center of array
                 left += 1
                 right -= 1
@@ -26,9 +26,14 @@ class Solution:
                 while left < right and sorted_nums[right] == sorted_nums[right + 1]:
                     right -= 1
 
-        return pairs
+        return counts
 
 
-nums = [5, 7, 9, 13, 11, 6, 6, 3, 3]
 s = Solution()
+# 3
+nums = [5, 7, 9, 13, 11, 6, 6, 3, 3]
+print(s.distinct_pairs(nums, 12))
+
+# 2
+nums = [6, 6, 3, 9, 3, 5, 1]
 print(s.distinct_pairs(nums, 12))
