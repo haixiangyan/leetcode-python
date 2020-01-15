add25d696e244b4163db4863b6c416f12991e98b
Haixiang6123 <haixiang6123@gmail.com>
2020-01-14 16:42:59 -0800

    Update 2. Spreadsheet Notation Conversion

--- VMWare OA/2. Spreadsheet Notation Conversion.py
+++ VMWare OA/2. Spreadsheet Notation Conversion.py
@@ -1,26 +1,44 @@
+import math
+
 class Solution:
+    def __init__(self):
+        self.COLS = 702
+
     def notation(self, target):
         if target <= 0:
             return None
 
-        cols = 702
-
-        row = target // cols + (0 if target % cols == 0 else 1)
-        col = (target % cols) if (target % cols) else cols
+        # Get row number and col number
+        row = math.ceil(target / self.COLS)
+        col = (target - 1) % self.COLS + 1
 
         col_str = self.get_col_str(col)
 
         return str(row) + col_str
 
+    # Transfer column number to string.
+    # Example: 702 -> 'ZZ'
     def get_col_str(self, col):
         col_str = ''
 
         while col > 0:
-            col_str = chr(ord('A') + ((col - 1) % 26)) + col_str
-            col = (col - 1) // 26
+            quotient, remainder = divmod(col - 1, 26)
+
+            col_str = chr(ord('A') + remainder) + col_str
+            col = quotient
 
         return col_str
 
-target = 702
 s = Solution()
+
+# 1ZZ
+target = 702
+print(s.notation(target))
+
+# 2A
+target = 703
+print(s.notation(target))
+
+# 1AA
+target = 27
 print(s.notation(target))
