cce3b2d3c3e74c788306a7fbeebfa3f7ca0a5539
Haixiang6123 <haixiang6123@gmail.com>
2020-01-14 16:44:37 -0800

    Add comment

--- VMWare OA/1. Distinct Pairs.py
+++ VMWare OA/1. Distinct Pairs.py
@@ -13,12 +13,16 @@ class Solution:
             elif sorted_nums[left] + sorted_nums[right] < target:
                 left += 1
             else:
+                # Add pair
                 pairs.append((sorted_nums[left], sorted_nums[right]))
+                # Move left and right to the center of array
                 left += 1
                 right -= 1
 
+                # Skip duplicate number on left hand side
                 while left < right and sorted_nums[left - 1] == sorted_nums[left]:
                     left += 1
+                # Skip duplicate number on right hand side
                 while left < right and sorted_nums[right] == sorted_nums[right + 1]:
                     right -= 1
 
