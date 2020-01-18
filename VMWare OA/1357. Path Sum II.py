89306520b127139743eba8c6a50b9ffbc17f37b0
Haixiang6123 <haixiang6123@gmail.com>
2020-01-18 11:26:24 -0800

    Solved 1357. Path Sum II

---
+++ VMWare OA/1357. Path Sum II.py
@@ -0,0 +1,30 @@
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+
+class Solution:
+    """
+    @param root: a binary tree
+    @param sum: the sum
+    @return: the scheme
+    """
+    def pathSum(self, root, target):
+        paths = []
+
+        self.dfs(root, [], target, paths)
+
+        return paths
+
+    def dfs(self, root, path, target, paths):
+        curt_path = path + [root.val]
+
+        if root.left is None and root.right is None:
+            if target - root.val == 0:
+                paths.append(curt_path)
+            return
+
+        if root.left:
+            self.dfs(root.left, curt_path, target - root.val, paths)
+        if root.right:
+            self.dfs(root.right, curt_path, target - root.val, paths)
