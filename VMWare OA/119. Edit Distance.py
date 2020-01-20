c9b20f8a015865e71a5120b1d1f66f731788b6ca
Haixiang6123 <haixiang6123@gmail.com>
2020-01-20 09:16:30 -0800

    Solved 119. Edit Distance

---
+++ VMWare OA/119. Edit Distance.py
@@ -0,0 +1,25 @@
+class Solution:
+    """
+    @param word1: A string
+    @param word2: A string
+    @return: The minimum number of steps.
+    """
+    def minDistance(self, word1, word2):
+        n, m = len(word1), len(word2)
+
+        dp = [[0 for _ in range(m + 1)] for _ in range(n + 1)]
+
+        for i in range(n + 1):
+            dp[i][0] = i
+
+        for j in range(m + 1):
+            dp[0][j] = j
+
+        for i in range(1, n + 1):
+            for j in range(1, m + 1):
+                if word1[i - 1] == word2[j - 1]:
+                    dp[i][j] = min(dp[i - 1][j - 1], dp[i - 1][j] + 1, dp[i][j - 1] + 1)
+                else:
+                    dp[i][j] = min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]) + 1
+
+        return dp[-1][-1]
