4d90f82fd95be04b18061adc350579acb8529c7b
Haixiang6123 <haixiang6123@gmail.com>
2020-01-24 09:37:24 -0800

    Refactor

--- VMWare/928. Longest Substring with At Most Two Distinct Characters.py
+++ VMWare/928. Longest Substring with At Most Two Distinct Characters.py
@@ -4,20 +4,20 @@ class Solution:
     @return: the length of the longest substring T that contains at most 2 distinct characters
     """
     def lengthOfLongestSubstringTwoDistinct(self, s):
-        most_left = {}
+        most_right = {}
         l, r = 0, 0
         longest = 0
 
         while r < len(s):
-            if len(most_left) <= 2:
-                most_left[s[r]] = r
+            if len(most_right) <= 2:
+                most_right[s[r]] = r
                 r += 1
-            if len(most_left) > 2:
+            if len(most_right) > 2:
                 left = len(s)
                 # Find most left
-                for value in most_left.values():
+                for value in most_right.values():
                     left = min(left, value)
-                most_left.pop(s[left])
+                    most_right.pop(s[left])
                 l = left + 1
             longest = max(longest, r - l)
 
