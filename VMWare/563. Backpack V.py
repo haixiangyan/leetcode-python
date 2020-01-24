3b9b949a2414f0de00c2b30fdd1ed15d7e5d6e11
Haixiang6123 <haixiang6123@gmail.com>
2020-01-24 13:13:31 -0800

    Solved 563. Backpack V

---
+++ VMWare/563. Backpack V.py
@@ -0,0 +1,15 @@
+class Solution:
+    """
+    @param nums: an integer array and all positive numbers
+    @param target: An integer
+    @return: An integer
+    """
+    def backPackV(self, nums, target):
+        dp = [0 for _ in range(target + 1)]
+        dp[0] = 1
+
+        for num in nums:
+            for i in range(target, num - 1, -1):
+                dp[i] += dp[i - num]
+
+        return dp[-1]
