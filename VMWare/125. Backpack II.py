2b3fa919569bb43ef7850f3795e3eb239f71dd1c
Haixiang6123 <haixiang6123@gmail.com>
2020-01-24 10:32:45 -0800

    Solved 125. Backpack II

---
+++ VMWare/125. Backpack II.py
@@ -0,0 +1,19 @@
+class Solution:
+    """
+    @param m: An integer m denotes the size of a backpack
+    @param A: Given n items with size A[i]
+    @param V: Given n items with value V[i]
+    @return: The maximum value
+    """
+    def backPackII(self, m, A, V):
+        n = len(A)
+        dp = [[0 for _ in range(m + 1)] for _ in range(n + 1)]
+
+        for i in range(1, n + 1):
+            for j in range(1, m + 1):
+                if A[i - 1] > j:
+                    dp[i][j] = dp[i - 1][j]
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - A[i - 1]] + V[i - 1])
+
+        return dp[-1][-1]
