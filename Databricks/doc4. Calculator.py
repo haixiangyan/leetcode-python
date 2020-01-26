4b8f7950ff849dbd48f69cafffd835b603d4dd1e
Haixiang6123 <haixiang6123@gmail.com>
2020-01-26 10:07:31 -0800

    Solved doc4. Calculator

---
+++ Databricks/doc4. Calculator.py
@@ -0,0 +1,18 @@
+def calculator(str):
+    if not str:
+        return 0
+
+    result, num, sign = 0, 0, 1
+    str += '+'
+    for char in str:
+        if char.isdigit():
+            num = num * 10 + int(char)
+        else:
+            result += sign * num
+            sign = 1 if char == '+' else -1
+            num = 0
+
+    return result
+
+str = '2+3-999'
+print(calculator(str))
