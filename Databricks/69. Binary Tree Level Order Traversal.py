0166848b9fb66947250034dc8414bb3ae770203e
Haixiang6123 <haixiang6123@gmail.com>
2020-01-29 10:03:46 -0800

    Solved 69. Binary Tree Level Order Traversal

---
+++ Databricks/69. Binary Tree Level Order Traversal.py
@@ -0,0 +1,32 @@
+from collections import deque
+
+
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+
+class Solution:
+    """
+    @param root: A Tree
+    @return: Level order a list of lists of integer
+    """
+    def levelOrder(self, root):
+        if root is None:
+            return []
+
+        order = []
+        queue = deque([root])
+
+        while queue:
+            level = []
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                level.append(node.val)
+
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            order.append(level)
+        return order
