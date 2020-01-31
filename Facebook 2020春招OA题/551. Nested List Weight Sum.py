6eb2bf9921c4e07ad76241e86e53ba0603cbdd71
Haixiang6123 <haixiang6123@gmail.com>
2020-01-31 10:03:43 -0800

    Solved 551. Nested List Weight Sum

---
+++ Facebook 2020春招OA题/551. Nested List Weight Sum.py
@@ -0,0 +1,38 @@
+"""
+This is the interface that allows for creating nested lists.
+You should not implement it, or speculate about its implementation
+
+class NestedInteger(object):
+    def isInteger(self):
+        # @return {boolean} True if this NestedInteger holds a single integer,
+        # rather than a nested list.
+
+    def getInteger(self):
+        # @return {int} the single integer that this NestedInteger holds,
+        # if it holds a single integer
+        # Return None if this NestedInteger holds a nested list
+
+    def getList(self):
+        # @return {NestedInteger[]} the nested list that this NestedInteger holds,
+        # if it holds a nested list
+        # Return None if this NestedInteger holds a single integer
+"""
+
+
+class Solution(object):
+    # @param {NestedInteger[]} nestedList a list of NestedInteger Object
+    # @return {int} an integer
+    def depthSum(self, nestedList):
+        return self.dfs(nestedList, 1)
+
+    def dfs(self, nested_list, depth):
+        result = 0
+
+        for item in nested_list:
+            if item.isInteger():
+                result += depth * item.getInteger()
+            else:
+                result += self.dfs(item.getList(), depth + 1)
+
+        return result
+
