d22899a815d0414aae6fb608f4677a10a0814dd6
Haixiang6123 <haixiang6123@gmail.com>
2020-02-01 14:21:35 -0800

    Solved 74. First Bad Version

---
+++ Facebook 2020春招OA题/74. First Bad Version.py
@@ -0,0 +1,29 @@
+class SVNRepo:
+   @classmethod
+   def isBadVersion(cls, id):
+       pass
+       # Run unit tests to check whether verison `id` is a bad version
+       # return true if unit tests passed else false.
+# You can use SVNRepo.isBadVersion(10) to check whether version 10 is a
+# bad version.
+class Solution:
+    """
+    @param n: An integer
+    @return: An integer which is the first bad version.
+    """
+    def findFirstBadVersion(self, n):
+        left, right = 1, n
+        while left + 1 < right:
+            mid = (left + right) // 2
+
+            if SVNRepo.isBadVersion(mid):
+                right = mid
+            else:
+                left = mid
+
+        if SVNRepo.isBadVersion(left):
+            return left
+        if SVNRepo.isBadVersion(right):
+            return right
+
+        return -1
