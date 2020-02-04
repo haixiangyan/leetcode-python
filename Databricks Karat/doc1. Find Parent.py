574c5373139fee14998343dd3e10349b129f5d31
Haixiang6123 <haixiang6123@gmail.com>
2020-02-04 08:57:57 -0800

    Solved doc1. Find Parent

--- Databricks Karat/doc1. Find Parent.py
+++ Databricks Karat/doc1. Find Parent.py
@@ -1,23 +1,18 @@
 # Time complexity: O(n)
 # Space complexity: O(n)
 def find_parent(edges):
-    if not edges:
-        return []
-
-    parents = {}
+    graph = {}
     for edge in edges:
         parent, child = edge
-        if child not in parents:
-            parents[child] = 0
-        if parent not in parents:
-            parents[parent] = 0
-        parents[child] += 1
 
-    results = []
-    for child in parents:
-        if parents[child] <= 1:
-            results.append(child)
-    return results
+        if child not in graph:
+            graph[child] = set()
+        if parent not in graph:
+            graph[parent] = set()
+
+        graph[child].add(parent)
+
+    return [node for node in graph if len(graph[node]) <= 1]
 
 edges = [[1, 4], [1, 5], [2, 5], [3, 6], [6, 7]]
 print(find_parent(edges))
