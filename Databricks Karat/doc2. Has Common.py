86b88b84ccc9194a41aa29b2e58e49527937c901
Haixiang6123 <haixiang6123@gmail.com>
2020-02-04 09:05:29 -0800

    Solved doc2. Has Common

--- Databricks Karat/doc2. Has Common.py
+++ Databricks Karat/doc2. Has Common.py
@@ -1,16 +1,24 @@
 def has_common(edges, a, b):
     graph = get_graph(edges)
 
-    a_parents = set()
-    b_parents = set()
+    a_parents = {a}
+    b_parents = {b}
 
     get_parents(graph, a, a_parents)
     get_parents(graph, b, b_parents)
 
-    for a_p in a_parents:
-        if a_p in b_parents:
+    for parent in a_parents:
+        if parent in b_parents:
             return True
+
     return False
+
+def get_parents(graph, node, parents):
+    if node not in graph:
+        return
+
+    for parent in graph[node]:
+        parents.add(parent)
 
 def get_graph(edges):
     graph = {}
@@ -18,23 +26,14 @@ def get_graph(edges):
     for edge in edges:
         parent, child = edge
 
-        if child not in graph:
-            graph[child] = []
-        graph[child].append(parent)
         if parent not in graph:
-            graph[parent] = []
+            graph[parent] = set()
+        if child not in graph:
+            graph[child] = set()
+
+        graph[child].add(parent)
 
     return graph
-
-def get_parents(graph, node, parents):
-    parents.add(node)
-
-    if not graph[node]:
-        return
-
-    for parent in graph[node]:
-        parents.add(parent)
-        get_parents(graph, parent, parents)
 
 edges = [[1, 4], [1, 5], [2, 5], [3, 6], [6, 7]]
 print(has_common(edges, 4, 5))
