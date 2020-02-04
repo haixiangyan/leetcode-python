9b8662b48af20762af6be54ec10af133f3d78f8d
Haixiang6123 <haixiang6123@gmail.com>
2020-02-04 10:23:24 -0800

    Review doc13. Domain Click

--- Databricks Karat/doc13. Domain Click.py
+++ Databricks Karat/doc13. Domain Click.py
@@ -2,6 +2,34 @@ def count_click(array):
     results = {}
 
     for item in array:
+        domains, counts = item
+
+        sub_domain = ''
+        sub_domains = domains.split('.')
+        for i in range(len(sub_domains) - 1, -1, -1):
+            domain = sub_domains[i]
+            if sub_domain == '':
+                sub_domain = domain
+            else:
+                sub_domain = domain + '.' + sub_domain
+
+            results[sub_domain] = results.get(sub_domain, 0) + int(counts)
+
+    return results
+
+
+array = [
+    ["google.com", "60"],
+    ["yahoo.com", "50"],
+    ["sports.yahoo.com", "80"]
+]
+print(count_click(array))
+
+
+def count_click(array):
+    results = {}
+
+    for item in array:
         domain, counts = item
         sub_domains = domain.split('.')
 
