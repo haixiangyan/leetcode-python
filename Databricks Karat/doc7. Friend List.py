396e067739ebf0f770895231567b7f90676cfe29
Haixiang6123 <haixiang6123@gmail.com>
2020-02-04 09:26:57 -0800

    Review doc7. Friend List

--- Databricks Karat/doc7. Friend List.py
+++ Databricks Karat/doc7. Friend List.py
@@ -1,14 +1,17 @@
 def friend_list(friendship_list):
-    result = {}
-    for f in friendship_list:
-        a, b = f.split(',')
-        if a not in result:
-            result[a] = []
-        if b not in result:
-            result[b] = []
-        result[a].append(b)
-        result[b].append(a)
-    return result
+    friends = {}
+
+    for friendship in friendship_list:
+        a, b = friendship.split(',')
+        if a not in friends:
+            friends[a] = set()
+        if b not in friends:
+            friends[b] = set()
+        friends[a].add(b)
+        friends[b].add(a)
+
+    return friends
+
 
 friendship_list = {
     "1,2",
