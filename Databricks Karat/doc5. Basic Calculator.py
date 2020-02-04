e2dd61b6fb984779138375602e0a274217261b50
Haixiang6123 <haixiang6123@gmail.com>
2020-02-04 09:16:21 -0800

    Review doc5. Basic Calculator

--- Databricks Karat/doc5. Basic Calculator.py
+++ Databricks Karat/doc5. Basic Calculator.py
@@ -1,36 +1,33 @@
 class Solution:
-    """
-    @param s: the given expression
-    @return: the result of expression
-    """
-    def calculate(self, s):
+    def calculate(self, s: str) -> int:
         if not s:
             return 0
 
-        result, num, sign = 0, 0, 1
+        result, number, sign = 0, 0, 1
         stack = []
+
         for char in s:
             if char.isdigit():
-                num = 10 * num + int(char)
+                number = 10 * number + int(char)
             elif char == '+':
-                result += sign * num
-                num = 0
-                sign = 1
+                result += sign * number
+                sign, number = 1, 0
             elif char == '-':
-                result += sign * num
-                num = 0
-                sign = -1
+                result += sign * number
+                sign, number = -1, 0
             elif char == '(':
                 stack.append(result)
                 stack.append(sign)
-                result = 0
-                sign = 1
+                result, sign = 0, 1
             elif char == ')':
-                result += sign * num
+                result += sign * number
+
                 result *= stack[-1]
                 stack.pop()
+
                 result += stack[-1]
                 stack.pop()
-                num = 0
-        result += sign * num
+
+                number, sign = 0, 1
+        result += sign * number
         return result
