b48b372abbe26f27991c4ce24d6bcb182fcb1d4b
Haixiang <haixiang6123@gmail.com>
2020-02-12 13:25:59 -0800

    Update The Skyline Problem

--- Databricks/131. The Skyline Problem.py
+++ Databricks/131. The Skyline Problem.py
@@ -24,7 +24,7 @@ class Solution:
         left_len, right_len = len(left_skyline), len(right_skyline)
         left = right = 0
         current_y = left_y = right_y = 0
-        skylines = []
+        skyline = []
 
         # 合并共有的部分
         while left < left_len and right < right_len:
@@ -41,14 +41,14 @@ class Solution:
             max_y = max(left_y, right_y)
             # 如果 y 值要变，要更新 skyline 了
             if max_y != current_y:
-                self.update_skyline(x, max_y, skylines)
+                self.update_skyline(x, max_y, skyline)
                 current_y = max_y
 
         # 将剩下的部分加入到 skyline
-        self.append_skyline(left, left_len, left_skyline, current_y, skylines)
-        self.append_skyline(right, right_len, right_skyline, current_y, skylines)
+        self.append_skyline(left_skyline, left, left_len, current_y, skyline)
+        self.append_skyline(right_skyline, right, right_len, current_y, skyline)
 
-        return skylines
+        return skyline
 
     def update_skyline(self, x, y, skylines):
         # 没有这个点的时候就加入
@@ -58,11 +58,10 @@ class Solution:
         else:
             skylines[-1][1] = y
 
-    def append_skyline(self, index, length, rest_skyline, current_y, skylines):
-        while index < length:
-            x, y = rest_skyline[index]
-            index += 1
+    def append_skyline(self, rest_skyline, start, length, current_y, skyline):
+        for i in range(start, length):
+            x, y = rest_skyline[i]
             # 如果 y 值要变，要更新 skyline 了
             if current_y != y:
-                self.update_skyline(x, y, skylines)
+                self.update_skyline(x, y, skyline)
                 current_y = y
