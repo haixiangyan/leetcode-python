d9cb8fe013c538bca8ad5183315a039480e91697
Haixiang <haixiang6123@gmail.com>
2020-02-11 17:37:06 -0800

    Update 392. House Robber

--- Databricks/392. House Robber.py
+++ Databricks/392. House Robber.py
@@ -5,14 +5,17 @@ class Solution:
     """
 
     def houseRobber(self, A):
-        if not A:
+        if len(A) == 0:
             return 0
+        if len(A) <= 2:
+            return max(A)
 
         n = len(A)
-        dp = [0 for _ in range(n + 1)]
-        dp[1] = A[0]
+        dp = [0 for _ in range(n)]
+        dp[0] = A[0]
+        dp[1] = max(A[0], A[1])
 
-        for i in range(2, n + 1):
-            dp[i] = max(dp[i - 1], dp[i - 2] + A[i - 1])
+        for i in range(2, n):
+            dp[i] = max(dp[i - 1], dp[i - 2] + A[i])
 
         return dp[-1]
