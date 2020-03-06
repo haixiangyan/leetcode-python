4711f9171426984e5477dfda403bf804c7206087
Haixiang6123 <haixiang6123@gmail.com>
2020-03-06 14:46:29 -0800

    Solved 117. Populating Next Right Pointers in Each Node II

---
+++ Microsoft/117. Populating Next Right Pointers in Each Node II.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        if root is None:
+            return None
+
+        queue = deque([root])
+        while queue:
+            # Make a list
+            self.make_list(queue)
+
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        return root
+
+    def make_list(self, queue):
+        for i in range(len(queue) - 1):
+            queue[i].next = queue[i + 1]
