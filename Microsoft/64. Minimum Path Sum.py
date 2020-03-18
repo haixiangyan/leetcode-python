23fcccb6e9650ee4a3e68bd4ee77dde8b90cd5b4
Haixiang <haixiang6123@gmail.com>
2020-03-18 00:57:44 -0700

    64. Mi

---
+++ Microsoft/64. Minimum Path Sum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if i == 0 and j > 0:
+                    grid[i][j] += grid[i][j - 1]
+                elif j == 0 and i > 0:
+                    grid[i][j] += grid[i - 1][j]
+                elif i > 0 and j > 0:
+                    grid[i][j] += min(grid[i - 1][j], grid[i][j - 1])
+
+        return grid[-1][-1]
