ea1e6e89ff206db20f2bec69f4cbbc61b20cf336
Haixiang <haixiang6123@gmail.com>
2020-03-18 01:23:25 -0700

    173. Binary Search Tree Iterator

---
+++ Microsoft/173. Binary Search Tree Iterator.py
@@ -0,0 +1,41 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class BSTIterator:
+
+    def __init__(self, root: TreeNode):
+        self.stack = []
+        while root:
+            self.stack.append(root)
+            root = root.left
+
+
+    def next(self) -> int:
+        node = self.stack[-1]
+        if node.right:
+            n = node.right
+            while n:
+                self.stack.append(n)
+                n = n.left
+        else:
+            n = self.stack.pop()
+            while self.stack and self.stack[-1].right == n:
+                n = self.stack.pop()
+
+        return node.val
+
+
+
+    def hasNext(self) -> bool:
+        return len(self.stack) > 0
+
+
+
+# Your BSTIterator object will be instantiated and called as such:
+# obj = BSTIterator(root)
+# param_1 = obj.next()
+# param_2 = obj.hasNext()
