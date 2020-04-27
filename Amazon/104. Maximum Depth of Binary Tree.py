b2b56aaf94f18f3f7fb79c455b93bbee68a3b5a4
Haixiang <haixiang6123@gmail.com>
2020-04-26 19:37:44 -0700

    104. Maximum Depth of Binary Tree

---
+++ Amazon/104. Maximum Depth of Binary Tree.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if root is None:
+            return 0
+
+        left = self.maxDepth(root.left)
+        right = self.maxDepth(root.right)
+
+        return max(left, right) + 1
