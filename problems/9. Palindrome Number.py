3585553698e96cb5eb4eb62f6559e8d1a63f428a
tx-haixiang <haixiangyan@tencent.com>
2020-08-02 16:32:08 +0800

    9. Palindrome Number

--- problems/9. Palindrome Number.py
+++ problems/9. Palindrome Number.py
@@ -1,3 +1,15 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        return str(x) == str(x)[::-1]
+        if x < 0:
+            return False
+
+        input_num = x
+        reverted_num = 0
+        while x > 0:
+            remainder = x % 10
+
+            reverted_num = reverted_num * 10 + remainder
+
+            x = x // 10
+
+        return reverted_num == input_num
