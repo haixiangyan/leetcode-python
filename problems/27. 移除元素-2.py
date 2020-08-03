a867bf556302a1cbdba0d9a49b777665d38a0962
Haixiang <haixiang6123@gmail.com>
2020-08-04 00:02:30 +0800

    27. 移除元素

---
+++ problems/27. 移除元素-2.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        if not nums:
+            return 0
+
+        i, n = 0, len(nums)
+        while i < n:
+            if nums[i] == val:
+                nums[i] = nums[n - 1]
+                n -= 1
+            else:
+                i += 1
+
+        return n
