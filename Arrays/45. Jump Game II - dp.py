8c556efcb095838a9d44e3a8ccf5c38f952e46f8
tx-haixiang <haixiangyan@tencent.com>
2020-08-20 00:50:17 +0800

    45. Jump Game II

---
+++ Arrays/45. Jump Game II - dp.py
@@ -0,0 +1,16 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        if not nums:
+            return -1
+
+        n = len(nums)
+        dp = [float('inf')] * n
+        dp[0] = 0
+
+        for i in range(1, n):
+            for j in range(i):
+                if j + nums[j] >= i:
+                    dp[i] = min(dp[i], dp[j] + 1)
+                    break
+                    
+        return dp[-1]
